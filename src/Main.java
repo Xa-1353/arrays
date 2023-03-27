0f58af90d086bcc2b0d38c562b3b8f24b5352d59
X-1213 <xaria.shella@gmail.com>
2023-03-26 19:03:21 -0500

    need to do frq

--- src/Main.java
+++ src/Main.java
@@ -26,5 +26,39 @@ public class Main {
         for(Integer item:listOfNums){
             System.out.println(item);
         }
+
+        System.out.println("Practice begins");
+        ArrayList<String> sam;
+        sam = new ArrayList<String>();
+        sam.add("ben");
+        sam.add(0, "wil");
+        sam.set(1,"cat");
+        sam.add(1,"dog");
+        sam.add("pig");
+        System.out.println(sam.set(1,"up"));
+        sam.remove(1);
+        sam.add("one");
+        sam.add(0,"five");
+        sam.remove(1);
+        System.out.println("new");
+        sam.add(0,"chair");
+        for(int i = 0; i < sam.size(); i++)
+            System.out.print(sam.get(i) + " ");
+        sam.remove(sam.size()-1);
+        System.out.println(sam);
+        for(int i = 0; i<4; i+=2)
+            sam.add(i, "fun");
+        System.out.println(sam);
+
+        System.out.println("TEST BEGINS");
+        ArrayList<Integer> rayList;
+        public int numCount(ArrayList<Integer> rayList, int num){
+            num = 0;
+            if (rayList.contains(num)){
+                num =
+            }
+
+        }
+
     }
 }
